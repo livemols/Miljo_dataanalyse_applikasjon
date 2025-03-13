0ab163d4e6dff8723755f195a7e8d968f8428c46
nathaldu <nathaldu@stud.ntnu.no>
2025-03-13 13:26:25 +0100

    fikset feil i månedstabell

--- data/månedstabell.py
+++ data/månedstabell.py
@@ -21,41 +21,48 @@ header=["Dato","Maksimumtemperatur", "Minimumtemperatur", "Nedbør(døgn)", "Hø
 #from testnat.py ^^^^
 
 
-#Make list with average values for each month
-def list_for_data():
-    for i in range(100): #antall rader
-        split.data_dict
 
 #Get the average values for the different months in the data set.
 def average_monthly():
     i=0
-    max_temp=0
-    monthly_average=()
+    monthly_average=[]
 
-    for i in range(len(data_list)):
+    while i < len(data_list):
         days_in_month=0
         max_temp, min_temp, rain, wind, snow= 0,0,0,0,0
-        while data_list[i][0][3:] == data_list[i+1][0][3:]:
-            max_temp+=int(data_list[i][1])
-            min_temp+=int(data_list[i][2])
-            rain+=int(data_list[i][3])
-            wind+=int(data_list[i][4])
-            snow+=int(data_list[i][5])
-            days_in_month+=1
-            date,month,year=str(data_list[i][0]).split(".")
-            month=int(month)
-            date=int(date)
-            year=int(year)
+        current_date=data_list[i][0]
+        current_day, current_month, current_year=current_date.split(".")
+
+        while i < len(data_list):
+            next_date=data_list[i][0]
+            next_day, next_month, next_year=next_date.split(".")
+            
+
+
+            if current_month==next_month and current_year==next_year:
+                max_temp+=int(data_list[i][1])
+                min_temp+=int(data_list[i][2])
+                rain+=int(data_list[i][3])
+                wind+=int(data_list[i][4])
+                snow+=int(data_list[i][5])
+                days_in_month+=1
+                i+=1
+
+            else:
+                break
+          
         avg_max_temp=max_temp/days_in_month
         avg_min_temp=min_temp/days_in_month
         avg_rain=rain/days_in_month
         avg_wind=wind/days_in_month
         avg_snow=snow/days_in_month
 
-        monthly_average.append(month,avg_max_temp,avg_min_temp,avg_rain,avg_wind,avg_snow)
-    return monthly_average
+        li=[current_month,avg_max_temp,avg_min_temp,avg_rain,avg_wind,avg_snow]
+        monthly_average.append(li)
+        
+    print (tabulate(monthly_average,headers=header,tablefmt="fancy_grid"))
 
-print(average_monthly())
+average_monthly()
 
         
 
