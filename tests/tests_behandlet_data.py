a5ad6c5beeb0efca0c60c2ca1dd77190f24ed068
“annamegg” <annamegg@stud.ntnu.no>
2025-03-26 12:50:40 +0100

    Adding Outlier removal and Time series data processing

--- tests/tests_behandlet_data.py
+++ tests/tests_behandlet_data.py
@@ -9,7 +9,7 @@ class test_behandlet_data(unittest.TestCase):
 
     def setUp(self):
         #oppretter testlisten med én dyplikat
-        self.df=pd.DataFrame({
+        self.df = pd.DataFrame({
             "Navn": ["Oslo","Oslo","Oslo"],
             "Stasjon": ["SN18700","SN18700","SN18700"],
             "Tid": ["21.02.2025","22.02.2025","22.02.2025"],
@@ -23,20 +23,45 @@ class test_behandlet_data(unittest.TestCase):
 
         #ser at navnet til kolonnene blir korrekt
     def test_navnendring_kolonner(self):
-        df=self.df.copy()
+        df = self.df.copy()
         df.columns=self.forventede_kolonner
         self.assertEqual(list(df.columns),self.forventede_kolonner)
     
         #ser at tiden blir skrevet om korret til Y-M-D i stedet for D-M-Y
     def test_tid_omskriving(self):
-        df=self.df.copy()
+        df = self.df.copy()
         df["Tid"] = pd.to_datetime(df["Tid"], format="%d.%m.%Y")
         self.assertTrue(pd.api.types.is_datetime64_any_dtype(df["Tid"]))
 
     def test_duplikat_fjerning(self):
-        df=self.df.copy()
+        df = self.df.copy()
         df.drop_duplicates(inplace = True)
         self.assertEqual(len(df),len(self.df)-1)
+
+    def test_outlier_removal(self):
+        df = self.df.copy()
+
+        threshold = 3
+
+        for column in df.columns:
+            mean = df[column].mean()
+            std = df[column].std()
+            lower_limit = mean - threshold * std
+            upper_limit = mean + threshold * std
+            df[column] = df[column].where(df[column].between(lower_limit, upper_limit))
+
+        self.assertEqual(len(df),len(self.df)-1)
+
+
+    def test_time_series_data_processing(self): 
+        
+        df = self.df.copy()
+
+        for column in modified_file.columns:
+            df[column] = modified_file[column].interpolate(method='polynomial', order=2)
+
+        self.assertEqual(len(df),len(self.df)-1)
+
 
 if __name__ == "__main__":
     unittest.main()
