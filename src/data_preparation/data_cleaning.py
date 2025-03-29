7804d4e7ec3e584e2543d4d02478f70e94a83634
nathaldu <nathaldu@stud.ntnu.no>
2025-03-29 13:53:55 +0100

    class data cleaning

--- src/data_preparation/data_cleaning.py
+++ src/data_preparation/data_cleaning.py
@@ -1,70 +1,58 @@
-#Denne filen renser og behandler dataen vår
+#Denne filen lager klassen data_cleaning som brukes til å rense data
 
 import pandas as pd
-import os
 import numpy as np
 
-# Finn absolutt sti til data-mappen
-current_dir = os.path.dirname(os.path.abspath(__file__))  # Finner mappen der testnat.py er
-data_path = os.path.join(current_dir, "..","..", "data")  # Går én mappe opp, deretter inn i data/
+class data_cleaning:
+    def form(df):
+        # Change the headers of the columm for better overview, and change the date to datetime format
+        df.columns = ["Navn", "Stasjon", "Tid", "Makstemp", "Mintemp", "Nedbør", "Vind", "Snø"]
+        df["Tid"] = pd.to_datetime(df["Tid"], format="%d.%m.%Y")
 
-# Spesifiser filnavnet
-filename = "blindern.csv"  
-
-# Lag full sti til filen
-original_file = os.path.join(data_path, filename)
-modified_file = os.path.join(data_path, "blindern_behandlet.csv")  
-
-# Read the original CSV file
-df = pd.read_csv(original_file, delimiter=";")
-
-# Apply modifications
-df.columns = ["Navn", "Stasjon", "Tid", "Makstemp", "Mintemp", "Nedbør", "Vind", "Snø"]
-df["Tid"] = pd.to_datetime(df["Tid"], format="%d.%m.%Y")
-
-# Convert columns to numeric
-df[["Makstemp", "Mintemp", "Nedbør", "Vind", "Snø"]] = df[["Makstemp", "Mintemp", "Nedbør", "Vind", "Snø"]].apply(pd.to_numeric, errors='coerce')
-
-#Delete duplitcates (rows)
-df.drop_duplicates(inplace = True)
-
-
-#Behandler manglende data ved interpolate
-#Endrer alle mising value "-" til Nan og deretter bruker interpolate
-df = df.replace("-", np.nan) 
-
-df=df.round(1)
-
-
-
-
-# Outlier removal
-
-threshold = 3
-
-for column in df.select_dtypes(include=['number']).columns:
-    mean = df[column].mean()
-    std = df[column].std()
-    lower_limit = mean - threshold * std
-    upper_limit = mean + threshold * std
-
-    # ".where" for å beholde NaN i stedet for å fjerne verdier helt
-    df[column] = df[column].where(df[column].between(lower_limit, upper_limit))
-
-
-# Time series data processing + missing data
-
-for column in df.select_dtypes(include=['number']).columns:
-    if column in ["Makstemp", "Mintemp"]:
-        df[column] = df[column].interpolate(method='polynomial', order=2).round(1)
-    if column in ["Nedbør", "Vind", "Snø"]:
-        df[column] = df[column].interpolate(method='linear').round(1)
-        df[column] = df[column].clip(lower = 0)
+        # Convert columns to numeric
+        df[["Makstemp", "Mintemp", "Nedbør", "Vind", "Snø"]] = df[["Makstemp", "Mintemp", "Nedbør", "Vind", "Snø"]].apply(pd.to_numeric, errors='coerce')
+    
+    def duplicates(df):
+        #Delete duplitcates (rows)
+        df.drop_duplicates(inplace = True)
+    
+    def missing(df):
+        #convert all "-" to Nan
+        df = df.replace("-", np.nan) 
         
+    
+    def outliers(df):
+        threshold = 3
+
+        for column in df.select_dtypes(include=['number']).columns:
+            mean = df[column].mean()
+            std = df[column].std()
+            lower_limit = mean - threshold * std
+            upper_limit = mean + threshold * std
+
+            # ".where" for å beholde NaN i stedet for å fjerne verdier helt
+            df[column] = df[column].where(df[column].between(lower_limit, upper_limit))
+
+    def replacing(df):
+        # Time series data processing + missing data
+
+        for column in df.select_dtypes(include=['number']).columns:
+            if column in ["Makstemp", "Mintemp"]:
+                df[column] = df[column].interpolate(method='polynomial', order=2).round(1)
+            if column in ["Nedbør", "Vind", "Snø"]:
+                df[column] = df[column].interpolate(method='linear').round(1)
+                df[column] = df[column].clip(lower = 0)
 
 
-# Save the modified DataFrame as a new CSV file in the same folder
-df.to_csv(modified_file, index=False, sep=";")  # Keep the same delimiter (;)
 
-print(f"Modified CSV file saved as: {modified_file}")
+
+
+
+
+
+
+
+
+
+
 
