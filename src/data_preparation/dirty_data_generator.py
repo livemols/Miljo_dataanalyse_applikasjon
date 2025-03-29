322f6775e907b105c0e995112655310ab7f8d38b
nathaldu <nathaldu@stud.ntnu.no>
2025-03-29 14:30:11 +0100

    språk endring

--- src/data_preparation/dirty_data_generator.py
+++ src/data_preparation/dirty_data_generator.py
@@ -1,42 +1,40 @@
-#Siden dataen vår, blindern.csv, inneholder lite feil som missing values og duplikater, skal vi i denne filen addere noen feil i en ny cvs.
-
-#Denne filen renser og behandler dataen vår
+#Since our data, blindern.csv, contains few mistakes like missing values or duplikates, will we in this file add some mistakes in a new csv-file.
+# This file cleans and treats our data
 import pandas as pd
 import os
 import numpy as np
 import numpy.ma as ma
 
-# Finn absolutt sti til data-mappen
-current_dir = os.path.dirname(os.path.abspath(__file__))  # Finner mappen der testnat.py er
-data_path = os.path.join(current_dir, "..","..", "data")  # Går én mappe opp, deretter inn i data/
+#Find absolute path til data-file
+current_dir = os.path.dirname(os.path.abspath(__file__))  # Finds folder where this file is
+data_path = os.path.join(current_dir, "..","..", "data")  # Go one folder up, then into data/
 
-# Spesifiser filnavnet
+# Spesifi the filename
 filename = "blindern.csv"  
 
-# Lag full sti til filen
+# Make full path til the file
 original_file = os.path.join(data_path, filename)
 modified_file = os.path.join(data_path, "blindern_dirty_data_generator.csv")  
 
-# leser rådata
+# read raw data
 df = pd.read_csv(original_file,skipfooter=1, engine='python',delimiter=";")
 
 
 
-
-#Adderer flere missing value "-", siden det var allerede slik fra før, i dataen
-#vil at 30% av radene skal endres
+#Add more missing value "-", because there were already "-" in the file.
+#want 30% of the rows to change 
 frac = 0.3
 
-#velger en random rad og columm og endrer tallet til "-"
+#choose a random row and column and change the number to "-" 
 idx = np.random.choice(range(df.shape[0]), int(df.shape[0]*frac), replace=True)
 cols = np.random.choice([3,4,5,6,7], size=len(idx), replace=True)
 x = df.to_numpy()
 x[idx, cols] = "-"
 df=pd.DataFrame(x, index=df.index, columns=df.columns)
 
+#ADD DUPLICATES 
+#choose the first 49 rows and add then to the end of DataFrame
 
-#ADDERER DUPLIKATER
-#velger de første 49 radene og addere de på slutten av DataFrame
 rows_to_duplicate=list(range(1,50))
 duplicates = df.iloc[rows_to_duplicate]  
 df = pd.concat([df, duplicates], ignore_index=True)
@@ -44,14 +42,11 @@ df = pd.concat([df, duplicates], ignore_index=True)
 
 
 
-
-#ADDERER OUTLIERS
-
-#lager en liste med outliers
+#ADD OUTLIERS
+#make a list with outliers
 outliers = np.array([200.5, 220.3, -500,-301, 400.5, 420.3, -200,-201])
 
-
-#velger en random rad og columm og endrer tallet til en fra outliers
+#choose a random row and column and change the number til one from outliers
 idx1 = np.random.choice(range(df.shape[0]), int(df.shape[0]*frac), replace=True)
 cols1 = np.random.choice([3,4,5,6,7], size=len(idx1), replace=True)
 x1 = df.to_numpy()
@@ -61,9 +56,8 @@ df=pd.DataFrame(x1, index=df.index, columns=df.columns)
 
 
 
-
-# lagrer DataFrame til ny cvs fil i samme mappe
-df.to_csv(modified_file, index=False, sep=";")  # Holder den samme delimiter (;)
+#make DataFrame to new csv file to the same folder
+df.to_csv(modified_file, index=False, sep=";")  # Keep the same delimiter (;)
 
 
 
